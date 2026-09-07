a6c3916c3922f08db965e0c403fb0c0b87c26461
Sultan Almaghthawi <thesultan.eid@gmail.com>
2026-09-07 20:14:22 +0300

    Initialize threatforge package

---
+++ src/threatforge/__init__.py
@@ -0,0 +1,10 @@
+"""
+ThreatForge - A Threat Modeling Tool
+
+This module provides threat modeling and analysis capabilities.
+"""
+
+__version__ = "0.1.0"
+__author__ = "SultanEid"
+
+# Package initialization
